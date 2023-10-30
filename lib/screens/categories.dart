c42d52d8ea951886b73531d705033b9d45047e27
damian-rog <damian.rog@quantoo.pl>
2023-10-30 19:07:36 +0100

    navigation:  new route created

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/category.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    // navigation in flutter woks as screens stack - as a user you alwyas see the top most screen; pushing new screen an the top:
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => MealsScreen(title: 'Basic Title', meals: []),
+      ),
+    );
+  }
 
   @override
   Widget build(context) {
@@ -20,7 +32,14 @@ class CategoriesScreen extends StatelessWidget {
           mainAxisSpacing: 20,
         ),
         children: availableCategories
-            .map((category) => CategoryGridItem(category: category))
+            .map(
+              (category) => CategoryGridItem(
+                category: category,
+                onSelectCategory: () {
+                  _selectCategory(context, category);
+                },
+              ),
+            )
             .toList(),
       ),
     );
